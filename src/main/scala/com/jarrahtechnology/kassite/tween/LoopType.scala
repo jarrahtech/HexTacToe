0ca84bb2c8073bff1fab7be398f7ecdc2d0ae71d
charles <cordincpublic@gmail.com>
2023-04-25 21:36:49 +0800

    repeat X times & end at right place

--- src/main/scala/com/jarrahtechnology/kassite/tween/LoopType.scala
+++ src/main/scala/com/jarrahtechnology/kassite/tween/LoopType.scala
@@ -3,25 +3,31 @@ package com.jarrahtechnology.kassite.tween
 import scala.concurrent.duration._
 import com.jarrahtechnology.util.Math._
 
-trait LoopType(val progress: Duration => Duration => Double, val hasFinished: Duration => Duration => Boolean)
+type ProgressFn = Duration => Duration => Double
+type FinishedFn = Duration => Duration => Boolean
+
+class LoopType(val progress: ProgressFn, val hasFinished: FinishedFn)
 
 object LoopType {
 
-  def runForever: Duration => Duration => Boolean = _ => _ => false
-  def stopAfterDuration: Duration => Duration => Boolean = duration => runTime => runTime>=duration
-  def stopAfterRepeating(times: Int): Duration => Duration => Boolean = duration => runTime => runTime.toNanos/duration.toNanos>=times
+  def runForever: FinishedFn = _ => _ => false
+  def stopAfterDuration: FinishedFn = duration => runTime => runTime>=duration
+  def stopAfterRepeating(times: Int): FinishedFn = duration => runTime => runTime.toNanos/duration.toNanos>=times
 
-  def forward: Duration => Duration => Double = duration => runTime => runTime/duration
-  def reverse: Duration => Duration => Double = duration => runTime => 1- runTime/duration
-  def loopForwardFromStart: Duration => Duration => Double = duration => runTime => durationMod(runTime, duration)/duration
-  def loopForwardThenReverse: Duration => Duration => Double = duration => runTime => {
+  def forward: ProgressFn = duration => runTime => clamp01(runTime/duration)
+  def reverse: ProgressFn = duration => runTime => clamp01(1- runTime/duration)
+  def loopForwardFromStart: ProgressFn = duration => runTime => durationMod(runTime, duration)/duration
+  def loopForwardThenReverse: ProgressFn = duration => runTime => {
       val runPercent = durationMod(runTime, duration)/duration
       if (isEven(runTime/duration)) runPercent else 1-runPercent // backward
     }
+  def repeat(times: Int, underlying: ProgressFn, end: Double): ProgressFn = 
+    duration => runTime => if (stopAfterRepeating(times)(duration)(runTime)) end else underlying(duration)(runTime)
 
   case object Once extends LoopType(forward, stopAfterDuration)
-  final case class Repeat(val times: Int) extends LoopType(loopForwardFromStart, stopAfterRepeating(times))
   case object Reverse extends LoopType(reverse, stopAfterDuration)
-  case object Cycle extends LoopType(loopForwardFromStart, runForever)
-  case object PingPong extends LoopType(loopForwardThenReverse, runForever)
+  final case class Cycle(times: Int) extends LoopType(repeat(times, loopForwardFromStart, 1), stopAfterRepeating(times))
+  case object CycleForever extends LoopType(loopForwardFromStart, runForever)
+  final case class PingPong(times: Int) extends LoopType(repeat(times, loopForwardThenReverse, if (isEven(times)) 0d else 1d), stopAfterRepeating(times))
+  case object PingPongForever extends LoopType(loopForwardThenReverse, runForever)
 }
