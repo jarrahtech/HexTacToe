85d3baf9d5dbc5a2a782331e8c025d2f619e9a47
charles <cordincpublic@gmail.com>
2023-04-30 11:03:33 +0800

    update vite base to work on pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
 
 export default defineConfig({
-  base: "/HexTacToe/",
+  base: "/HexTacToe/dist/",
   plugins: [scalaJSPlugin()],
 });
