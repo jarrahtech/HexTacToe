e311edcf02740df528a20d167d3b71b2cae30720
charles <cordincpublic@gmail.com>
2023-04-30 10:46:10 +0800

    stop ignoring /dist (for github pages)

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
 
 export default defineConfig({
+  base: "/HexTacToe/",
   plugins: [scalaJSPlugin()],
 });
